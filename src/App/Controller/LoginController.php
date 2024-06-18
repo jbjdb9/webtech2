bb76e2e30812a8511e093f913222b614ffacd110
Robbie Kooistra <robbiekooistra2000@gmail.com>
2024-06-18 17:08:59 +0200

    Remove commented out code

--- src/App/Controller/LoginController.php
+++ src/App/Controller/LoginController.php
@@ -15,11 +15,6 @@ class LoginController
             $usernameOrEmail = $request->getPost('usernameOrEmail');
             $password = $request->getPost('password');
 
-//            if (empty($usernameOrEmail) || empty($password)) {
-//                $this->handleFailedLogin($response, 'Login failed: username or password not provided');
-//                return;
-//            }
-
             $user = ORM::getUserByUsernameOrEmail($usernameOrEmail);
 
             if (!$user) {
@@ -35,28 +30,21 @@ class LoginController
             $_SESSION['userId'] = $user->getId();
             $_SESSION['role'] = UserRole::getRoleNameByUserId($user->getId());
 
-//            if (empty($_SESSION['userId'])) {
-//                $this->handleFailedLogin($response, 'Login failed: unable to store user ID in session');
-//                return;
-//            }
-
             error_log('User logged in, userId: ' . $_SESSION['userId'] . ', role: ' . $_SESSION['role']);
             $response->redirect('/home');
         } else {
             $response->setTemplate('login.php');
         }
     }
-
-    private function handleFailedLogin(Response $response, $errorMessage)
-    {
-        error_log($errorMessage);
-        $response->setTemplate('login.php', ['error' => $errorMessage]);
-    }
-
     public function logout(Request $request, Response $response)
     {
         session_destroy();
 
         $response->redirect('/login');
+    }
+    private function handleFailedLogin(Response $response, $errorMessage)
+    {
+        error_log($errorMessage);
+        $response->setTemplate('login.php', ['error' => $errorMessage]);
     }
 }
